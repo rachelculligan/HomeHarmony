567d42f0b4fe3ce153a5fd18b5d6e87dd5f3eaef
Rachel Culligan <rachelculligan@gmail.com>
2026-06-16 10:17:57 -0400

    Initial Commit

---
+++ CleaningADHDTests/CleaningADHDTests.swift
@@ -0,0 +1,19 @@
+//
+//  CleaningADHDTests.swift
+//  CleaningADHDTests
+//
+//  Created by Rachel Culligan on 6/16/26.
+//
+
+import Testing
+@testable import CleaningADHD
+
+struct CleaningADHDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
